d0cfa97e2564db103da51045b1833a1ffdcd200d
rstar2 <rstar2@abv.bg>
2018-02-20 11:26:18 +0200

    Allow Page slug to be explicitly specified

--- models/Page.js
+++ models/Page.js
@@ -8,11 +8,12 @@ const Types = keystone.Field.Types;
 
 const Page = new keystone.List('Page', {
 	map: { name: 'title' },
-	autokey: { path: 'slug', from: 'title', unique: true },
+	autokey: { path: 'slug', from: 'title', unique: true, fixed: true },
 });
 
 Page.add({
 	title: { type: String, required: true },
+	slug: { type: Types.Key }, // allow the slug to be explicitly specified
 	state: { type: Types.Select, options: 'draft, published, archived', default: 'draft', index: true },
 	author: { type: Types.Relationship, ref: 'User', index: true },
 	publishedDate: { type: Types.Date, index: true, dependsOn: { state: 'published' } },
