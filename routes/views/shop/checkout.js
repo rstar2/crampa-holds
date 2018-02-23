c7e3b647673eb6e9f4b19756ec73694eb20a0ad1
rstar2 <rstar2@abv.bg>
2018-02-23 16:49:11 +0200

    Start integrating PayPal gateway

--- routes/views/shop/checkout.js
+++ routes/views/shop/checkout.js
@@ -8,11 +8,32 @@ exports = module.exports = function (req, res) {
 	// Set locals
 	locals.section = 'shop';
 
-	// On POST requests, add the Enquiry item to the database
+	// PayPal Express Checkout
+	// https://www.youtube.com/watch?v=7k03jobKGXM
+	// https://github.com/paypal/PayPal-node-SDK
+	// https://developer.paypal.com/developer/accounts
+	// https://www.sandbox.paypal.com/signin 
 	view.on('post', function (next) {
+		// var newEnquiry = new Enquiry.model();
+		// var updater = newEnquiry.getUpdateHandler(req);
 
-		console.log();
+		// updater.process(req.body, {
+		// 	flashErrors: true,
+		// 	fields: 'name, email, phone, enquiryType, message',
+		// 	errorMessage: 'There was a problem submitting your enquiry:',
+		// }, function (err) {
+		// 	if (err) {
+		// 		locals.validationErrors = err.errors;
+		// 	} else {
+		// 		locals.enquirySubmitted = true;
+		// 	}
+		// 	next();
+		// });
+	});
 
+	// PayPal Classic Payment Button
+	// https://developer.paypal.com/docs/integration/web/
+	view.on('post', function (next) {
 		// var newEnquiry = new Enquiry.model();
 		// var updater = newEnquiry.getUpdateHandler(req);
 
