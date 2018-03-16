3cd54c35fa29b1b2e7a2c0580af03e68b5c216e4
rstar2 <rstar2@abv.bg>
2018-03-16 13:47:01 +0200

    No jQuery even in blog-editor

--- client/src/site/post/comments.js
+++ client/src/site/post/comments.js
@@ -15,31 +15,3 @@ new Vue({
 	},
 
 });
-
-
-// TODO: UIse Vue.js
-// jQuery(function ($) {
-// 	var comments = $('#comments');
-// 	var input = $('.comment-field-input');
-// 	var submit = comments.find('button[type=submit]');
-
-// 	// Scroll to comments and focus input field
-// 	$('[href="#comments"]').click(function scrollToComments(e) {
-// 		e.preventDefault();
-
-// 		$('html, body').animate({ scrollTop: comments.offset().top }, 250);
-
-// 		input.eq(0).focus();
-// 	});
-
-// 	// Check if field has content: enable/disable submit. Disable by default
-// 	submit.attr('disabled', 'disabled');
-// 	input.keyup(function () {
-// 		if ($.trim($(this).val())) {
-// 			submit.removeAttr('disabled');
-// 		} else {
-// 			submit.attr('disabled', 'disabled');
-// 		}
-// 	});
-// });
-
